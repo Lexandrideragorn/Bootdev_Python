b919c3662f90e374f5bc61bbf2125657dbdf6da5
Lexandrideragorn <abhorash84@gmail.com>
2026-09-25 14:36:27 +0100

    C4L1 - Asteroids - (Chaning naming convention to match Chapter and lesson.)

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,7 +1,9 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
-import player
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     #print(f"Starting Asteroids")
@@ -11,17 +13,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     deltaTime = 0.0
-    p1 = player.Player(SCREEN_WIDTH/ 2,SCREEN_HEIGHT/ 2)
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    p1 = Player(SCREEN_WIDTH/ 2,SCREEN_HEIGHT/ 2)
+    asteroidfield = AsteroidField()
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        p1.update(deltaTime)
+        #p1.update(deltaTime)
+        updatable.update(deltaTime)
         screen.fill("black")
-        p1.draw(screen)
+        #p1.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         deltaTime = clock.tick(60) / 1000
         #print(deltaTime)
